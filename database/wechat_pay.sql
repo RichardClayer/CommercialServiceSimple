5f1a2ca46a0f7294e6adc31d4174551bd28f464e
Unknown <junlingorg@gmail.com>
2019-06-25 21:46:35 +0800

    获取微信配置

--- database/wechat_pay.sql
+++ database/wechat_pay.sql
@@ -1,13 +1,15 @@
 -- 微信支付商户信息
 CREATE TABLE wechat_pay
 (
-  id              int PRIMARY KEY AUTO_INCREMENT,
-  app_id          varchar(255) NOT NULL COMMENT '微信公众号id',
-  app_secret      varchar(255) NOT NULL COMMENT '微信公众号密钥',
-  mch_id          varchar(255) NOT NULL COMMENT '微信商户号',
-  `key`           varchar(255) NOT NULL COMMENT '微信支付API安全密钥',
-  api_client_cert text COMMENT 'API安全证书公钥',
-  api_client_key  text COMMENT 'API安全证书密钥'
+  id              INT PRIMARY KEY AUTO_INCREMENT,
+  app_id          VARCHAR(255) NOT NULL COMMENT '微信公众号id',
+  app_secret      VARCHAR(255) NOT NULL COMMENT '微信公众号密钥',
+  mch_id          VARCHAR(255) NOT NULL COMMENT '微信商户号',
+  `key`           VARCHAR(255) NOT NULL COMMENT '微信支付API安全密钥',
+  api_client_cert TEXT COMMENT 'API安全证书公钥',
+  api_client_key  TEXT COMMENT 'API安全证书密钥',
+  merchant_id     INT  COMMENT '商户ID',
+  index wechat_pay_merchant_id_index (merchant_id)
 ) ENGINE = INNODB
   DEFAULT CHARACTER SET 'utf8'
   COLLATE utf8_general_ci
--- pkg/services/v1/merchant.go
+++ pkg/services/v1/merchant.go
@@ -1,1 +1,34 @@
 package v1
+
+import (
+    "context"
+    "fmt"
+    "github.com/BiLuoHui/CommercialServiceSimple/tool/database"
+)
+
+// 商户基础资料管理、微信商户配置、支付宝商户配置
+type WechatPayConfig struct {
+    Id            int    `json:"id"`
+    AppId         string `json:"app_id"`
+    AppSecret     string `json:"app_secret"`
+    MchId         string `json:"mch_id"`
+    Key           string `json:"key"`
+    APIClientCert string `json:"api_client_cert"`
+    APIClientKey  string `json:"api_client_key"`
+    MerchantId    int    `json:"merchant_id"`
+}
+
+// Get 获取微信支付配置
+func (w *WechatPayConfig) Get() error {
+    c, err := database.Connect()
+    if err != nil {
+        return fmt.Errorf("数据库链接失败：%v", err)
+    }
+    defer c.Close()
+    row := c.QueryRowContext(context.Background(), "select * from wechat_pay limit 1")
+    if err = row.Scan(&w.Id, &w.AppId, &w.AppSecret, &w.MchId, &w.Key, &w.APIClientCert, &w.APIClientKey, &w.MerchantId); err != nil {
+        return fmt.Errorf("获取微信支付配置失败：%v", err)
+    }
+
+    return nil
+}
