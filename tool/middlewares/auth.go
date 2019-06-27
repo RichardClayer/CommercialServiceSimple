f04db475cc8506b7efcbcce79fc09e77b791fd8e
碧落晖 <junlingorg@gmail.com>
2019-06-27 22:28:38 +0800

    format

--- tool/middlewares/auth.go
+++ tool/middlewares/auth.go
@@ -1,39 +1,40 @@
 package middlewares
 
 import (
-	"github.com/BiLuoHui/CommercialServiceSimple/config"
-	"github.com/BiLuoHui/CommercialServiceSimple/tool/auth"
-	"github.com/BiLuoHui/CommercialServiceSimple/tool/response"
-	"net/http"
+    "net/http"
+
+    "github.com/BiLuoHui/CommercialServiceSimple/config"
+    "github.com/BiLuoHui/CommercialServiceSimple/tool/auth"
+    "github.com/BiLuoHui/CommercialServiceSimple/tool/response"
 )
 
 // RequestAuth 是否需要登录验证
 func RequestAuth(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		path := r.RequestURI
-		if isNeed(path, config.NoNeedRoutes) {
-			token := r.Header.Get("Authorization")
-			if ok := auth.IsOnline(token); !ok {
-				rd := response.RespData{
-					Code:    response.NeedLogin,
-					Message: "请先登录",
-				}
-				response.Send(w, rd)
-				return
-			}
-		}
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        path := r.RequestURI
+        if isNeed(path, config.NoNeedRoutes) {
+            token := r.Header.Get("Authorization")
+            if ok := auth.IsOnline(token); !ok {
+                rd := response.RespData{
+                    Code:    response.NeedLogin,
+                    Message: "请先登录",
+                }
+                response.Send(w, rd)
+                return
+            }
+        }
 
-		next.ServeHTTP(w, r)
-	})
+        next.ServeHTTP(w, r)
+    })
 }
 
 // 是否需要登录认证
 func isNeed(path string, noNeed []string) bool {
-	for _, s := range noNeed {
-		if path == s {
-			return false
-		}
-	}
+    for _, s := range noNeed {
+        if path == s {
+            return false
+        }
+    }
 
-	return true
+    return true
 }
